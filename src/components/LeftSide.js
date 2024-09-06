a01bca08e78bbe4c29ca120157246a6d6f93f76a
Emre <soylmze@gmail.com>
2024-09-06 23:59:50 +0300

    content styles updated

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './LeftSide.css';
 import logo from '../images/logo.svg';
+import { ReactComponent as BtnSvg } from '../images/icon-arrow.svg';
 
 const LeftSide = () => {
   return (
@@ -9,8 +10,8 @@ const LeftSide = () => {
         <div className='logo-container'>
           <img src={logo} alt="Logo" className="img-fluid mb-3" />
         </div>
-        <h1 className="display-1">We're coming soon</h1>
-        <p className="lead fs-6 mt-2">
+        <h1 className="display-1 fw-700"><span className='text-pink fw-100'>We're</span> coming soon</h1>
+        <p className="lead fs-6 mt-2 text-pink fw-400">
           Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.
         </p>
       </div>
@@ -19,11 +20,11 @@ const LeftSide = () => {
           <input
             type="email"
             id="email"
-            placeholder="Enter your email"
+            placeholder="Email Address"
             required
-            className="form-control rounded-pill input-width"
+            className="form-control rounded-pill input-width text-pink"
           />
-          <button type="submit" className="btn btn-primary rounded-pill position-absolute">Subscribe</button>
+          <button type="submit" className="btn-bg rounded-pill position-absolute"><BtnSvg /></button>
         </div>
       </form>
     </div>
